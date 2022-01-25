9a35e8753fc1ba668eb39acb7d9a8ccb4b2d7a2d
jesrodri <82549482+jesrodri@users.noreply.github.com>
2022-01-25 10:34:26 -0300

    Update main.rb
    Ordered the method's names

--- main.rb
+++ main.rb
@@ -1,21 +1,21 @@
 class Main
+  def to_git1
+    "git2"
+  end
+  
+  def to_git2
+    "git2"
+  end
+  
+  def to_git3
+    "oila mundo"
+  end
+  
   def to_git4
     "git4"
   end
 
-  def to_git2
-    "git2"
-  end
-
-  def to_git3
-    "oila mundo"
-  end
-
   def to_git5
     "é isto ai, meu chapa"
-  end
-
-  def to_git1
-    "git2"
   end
 end
